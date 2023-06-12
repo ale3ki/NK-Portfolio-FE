9c2ee4ed3b54f263562ea2562397d87e3dbd0829
Anthony Lee <anthonykilde@outlook.com>
2023-06-12 18:49:34 -0500

    HOTFIX: Switched scroll anim to anime.js

--- components/main/TextScroll/component.tsx
+++ components/main/TextScroll/component.tsx
@@ -1,8 +1,9 @@
 "use client"
 import styles from './styles.module.css';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import { useApiService } from '../../../utils/ApiServiceContext';
 import { Container } from '../../../utils/ApiDataInterface';
+import anime from 'animejs';
 
 //Set this to change the loading string for the elements within the component.
 const loadingString: string = "Loading...    Loading...    Loading...    Loading...    ";
@@ -12,14 +13,14 @@ export default function TextScroll(props: { pageId: number, containerId: number,
     const [list, setList] = useState<string>(Array(2).fill(loadingString).join(''));
     const { pageId, containerId, bgColor, textColor, paddingBottom, paddingTop } = props;
     const apiService = useApiService();
+    const textRef = useRef<HTMLDivElement>(null);
 
     useEffect(() => {
         const fetchData = async () => {
             try {
-                //if this data is cached, it will use the cached data instead.  Everything is automated (hopefully).
                 const result = await apiService.getContainerDataByPageID(pageId, containerId);
-                const newText = Array(2).fill(result?.description || 'Error...').join('');
-                setList(newText);
+                const newText = result?.description || 'Error...';
+                setList(Array(2).fill(newText).join(''));
                 setData(result);
             } catch (error) {
                 console.error(error);
@@ -29,23 +30,37 @@ export default function TextScroll(props: { pageId: number, containerId: number,
         fetchData();
     }, [apiService]);
 
+    useEffect(() => {
+        if (textRef.current) {
+            const animation = anime({
+                targets: textRef.current,
+                translateX: ['-0%', '-50%'],
+                duration: 40000,
+                loop: true,
+                easing: 'linear'
+            });
+        
+            return () => animation.pause(); // clean up the animation if the component unmounts or textRef changes
+        }
+    }, [textRef]);
+    
+
     switch (data) {
         case (null):
             return <div>Whoops, there was a fatal error fetching the data.</div>;
-            default:
-                return (
-                  <div className={`${styles.scrollHolder}`} style={{
-                      paddingTop: paddingTop,
-                      paddingBottom: paddingBottom,
-                      background: bgColor
-                  }}>
+        default:
+            return (
+                <div className={`${styles.scrollHolder}`} style={{
+                    paddingTop: paddingTop,
+                    paddingBottom: paddingBottom,
+                    background: bgColor
+                }}>
                     <div className={styles.scroll}>
-                        <div className={styles.text} style={{ color: textColor }}>
+                        <div className={styles.text} ref={textRef} style={{ color: textColor }}>
                             {list}{list}
                         </div>
                     </div>
-                  </div>
-              );
-              
+                </div>
+            );
     }
 }
